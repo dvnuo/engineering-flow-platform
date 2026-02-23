19e09a7892a9917584b59e6537748856d0989d2b
VM-232 <vm-232.email@gmail.com>
2026-02-23 23:51:44 +0800

    refactor: Remove backward compatibility for api_token
    Now only supports:
    - token: Bearer token
    - password: Basic auth (username+password)

--- src/config.py
+++ src/config.py
@@ -206,9 +206,8 @@ class Config:
                 "url": jira_config.get("url", ""),
                 "project": jira_config.get("project", ""),
                 "username": jira_config.get("username", ""),
-                # Support old api_token, bearer_token -> new password, token
-                "password": jira_config.get("password") or jira_config.get("api_token", ""),
-                "token": jira_config.get("token") or jira_config.get("bearer_token", ""),
+                "password": jira_config.get("password", ""),
+                "token": jira_config.get("token", ""),
                 "api_version": jira_config.get("api_version", "3"),
                 "timeout": jira_config.get("timeout", 30.0),
             }]
@@ -254,8 +253,7 @@ class Config:
                 "name": "Default",
                 "url": confluence_config.get("url", ""),
                 "username": confluence_config.get("username", ""),
-                # Support old api_token -> new password (Basic Auth)
-                "password": confluence_config.get("password") or confluence_config.get("api_token", ""),
+                "password": confluence_config.get("password", ""),
                 "token": confluence_config.get("token", ""),
                 "space": confluence_config.get("space", ""),
             }]
