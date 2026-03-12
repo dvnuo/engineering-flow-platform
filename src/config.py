c0f64a9680a727c8034bc606fd43455db5010327
LucasLai <lan_ff@qq.com>
2026-03-12 13:30:16 +0800

    Add additional addresses to no_proxy configuration

--- src/config.py
+++ src/config.py
@@ -321,7 +321,7 @@ class Config:
             os.environ["HTTP_PROXY"] = url
             os.environ["HTTPS_PROXY"] = url
             # Handle no_proxy for internal addresses
-            no_proxy = proxy_config.get("no_proxy", "localhost,127.0.0.1")
+            no_proxy = proxy_config.get("no_proxy", "localhost,127.0.0.1,169.254.169.254,svc.cluster.local")
             os.environ["no_proxy"] = no_proxy
             os.environ["NO_PROXY"] = no_proxy
         elif "proxy" in self._config:
